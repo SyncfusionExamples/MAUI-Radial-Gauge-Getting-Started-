1d9dcedd15f9ae19206277cb7c8c513563d1a293
HadayaRahman-SF4673 <hadayarahman.m@syncfusion.com>
2025-04-16 13:20:54 +0530

    952707-Updated .NET Version

--- RadialGaugeGettingStarted/MainPage.xaml.cs
+++ RadialGaugeGettingStarted/MainPage.xaml.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Essentials;
-
-namespace RadialGaugeGettingStarted
+﻿namespace RadialGaugeGettingStarted
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
 }
--- RadialGaugeGettingStarted/MauiProgram.cs
+++ RadialGaugeGettingStarted/MauiProgram.cs
@@ -1,25 +1,26 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Controls.Compatibility;
-using Microsoft.Maui.Controls.Hosting;
+﻿using Microsoft.Extensions.Logging;
 using Syncfusion.Maui.Core.Hosting;
-
 namespace RadialGaugeGettingStarted
 {
-	public static class MauiProgram
-	{
-		public static MauiApp CreateMauiApp()
-		{
-			var builder = MauiApp.CreateBuilder();
-			builder
-				.UseMauiApp<App>()
-				.ConfigureSyncfusionCore()
-				.ConfigureFonts(fonts =>
-				{
-					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-			return builder.Build();
-		}
-	}
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
 }
